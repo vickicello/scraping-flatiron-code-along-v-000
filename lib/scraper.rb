941e2bd90a4d9b9ea550b48ab858a17565d02f00
Vicki Hamm <vicki.cello@gmail.com>
2018-07-03 18:56:22 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,8 +13,8 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
-      binding.pry
     end
+      binding.pry
   end
 
     def print_courses
